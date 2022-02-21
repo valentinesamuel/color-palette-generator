21f6940b18cd93db5b98ea70ab3fe08f26b6e652
valentinesamuel <valentinesamuel2580@gmail.com>
2022-02-21 01:34:57 +0100

    feat: corrected color card and added the nex code

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,7 +1,7 @@
 import "./Button.css";
 
 const Button = ({ getColors }) => {
-  return <button onClick={getColors}>Click to generate random colors</button>;
+  return <button onClick={getColors}>Generate Palette</button>;
 };
 
 export default Button;
