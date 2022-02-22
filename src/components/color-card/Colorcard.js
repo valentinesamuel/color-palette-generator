e5a396990ede4a984ce10e59f3f3b21b47613dfe
valentinesamuel <valentinesamuel2580@gmail.com>
2022-02-22 09:51:13 +0100

    feat: copying to clipboard
    nect: add animations and improve user experience

--- src/components/color-card/Colorcard.js
+++ src/components/color-card/Colorcard.js
@@ -1,23 +1,45 @@
-
+import { useEffect, useState } from "react";
 import "./Colorcard.css";
-
+import Prompt from '../prompt/Prompt'
+import Clipboard from 'react-clipboard.js';
 const ColorCard = ({ colors }) => {
 
+  const [copy, setCopy] = useState({ value: '', copied: false })
 
-  return (
-    <div className="card-row">
-      {colors.map((color, index) => {
-        return (
-          <div className="colorframe" key={index}>
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCopy({ copied: false })
+    }, 2000);
+    return () => clearTimeout(timeout)
+  }, [copy])
+
+  const onCopy = (color) => {
+    setCopy({ value: `#(${color})`, copied: true });
+  }
+
+  const renderCard = () => {
+    return colors.map((color, index) => {
+      return (
+        <Clipboard key={index} data-clipboard-text={`#(${color})`} onClick={() => { onCopy(color) }} component='a'>
+
+          <div className="colorframe" >
             <div
               className="colorcard"
               style={{ backgroundColor: `rgb(${color})` }}
             ></div>
             <p className="hex-color">#{color}</p>
           </div>
-        );
-      })}
-    </div>
+        </Clipboard>
+      );
+    })
+  }
+  return (
+    <>
+      {copy.copied ? <Prompt message={copy.value.replaceAll(',', '')} show={copy.copied} /> : null}
+      <div className="card-row">
+        {renderCard()}
+      </div>
+    </>
   );
 };
 
--- src/components/prompt/Prompt.js
+++ src/components/prompt/Prompt.js
@@ -1,7 +1,11 @@
 import './Prompt.css'
 
-const Prompt = ({message}) => {
-  return <div>{message}</div>;
+const Prompt = ({ message, show }) => {
+  return (
+    <>
+      {show ? <p className='prompt_message'>{message} Has been copied to your clipboard</p> : ''}
+    </>
+  );
 };
 
 export default Prompt;
